a6d167d648e0cf8623c0878d6e68fcd098604f3b
SHJ984 <dokdae0624@naver.com>
2024-02-29 17:20:50 +0900

    etty

--- sql/builtinfunction.sql
+++ sql/builtinfunction.sql
@@ -12,3 +12,22 @@ GROUP BY custid;
 -- 4-4
 SELECT bookid, REPLACE(bookname, '야구', '농구') bookname, publisher, price
 FROM Book;
+
+-- 4-5
+SELECT bookname '제목', CHAR_LENGTH(bookname) '문자수',
+	LENGTH(bookname) '바이트수'
+    FROM Book
+    WHERE publisher='굿스포츠';
+ -- 4-6
+ SELECT SUBSTR(name, 1, 1) '성', COUNT(*) '인원'
+ FROM Customer
+ GROUP BY SUBSTR(name, 1, 1);
+ -- 4-7
+ SELECT orderid '주문번호', orderdate '주문일',
+ ADDDATE(orderdate, INTERVAL 10 DAY) '확정'
+ FROM Orders;
+ -- 4-8
+ SELECT orderid '주문번호', DATE_FORMAT(orderdate, '%Y-%m-%d') '주문일',
+ custid '고객번호', bookid '도서번호'
+ FROM Orders
+ WHERE orderdate=STR_TO_DATE('20240707', '%Y%m%d');
