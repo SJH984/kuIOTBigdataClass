02503336f2d29f94906f6e63759775b18b3daa34
SHJ984 <dokdae0624@naver.com>
2024-03-08 10:07:19 +0900

    jgggh

--- python/booklist.c
+++ python/booklist.c
@@ -2,21 +2,10 @@
 #include <mysql.h>
 #include <string.h>
 // sudo apt install libmysql++*
-int main() {
+int main(){
     MYSQL *conn;
     MYSQL_RES *res;
-    MYSQL_EOW row;
-    char *host = "localhost";
-    char *user = "root";
-    char *passwd = "0000";
-    #include <stdio.h>
-#include <mysql.h>
-#include <string.h>
-// sudo apt install libmysql++*
-int main() {
-    MYSQL *conn;
-    MYSQL_RES *res;
-    MYSQL_EOW row;
+    MYSQL_ROW row;
     char *host = "localhost";
     char *user = "root";
     char *passwd = "0000";
@@ -27,49 +16,22 @@ int main() {
     strcpy(query, "select * from Book");
 
     conn = mysql_init(NULL);
-    
-    if(mysql_real_connect(conn, host, user, passwd, db, port, NULL, 0)) {
-        orintf("MySQL 연결 성공\n");
+    if(mysql_real_connect(conn, host, user, passwd, db, port, NULL, 0)){
+        printf("MySQL 연결 성공\n");
     }else{
-        printf("MYSQL 연결 실패\n");
+        printf("MySQL 연결 실패\n");
     }
+
     if(mysql_query(conn, query)){
         printf("쿼리 실패\n");
     }
-    
+
     res = mysql_store_result(conn);
     while(row = mysql_fetch_row(res)){
         printf("%s\t", row[0]);
         printf("%s\t", row[1]);
         printf("%s\t", row[2]);
         printf("%s\t\n", row[3]);
-    }
-    mysql_close(conn);
-    return 0;
- }
-    char *db = "madangdb";
-    char query[255];
-    int port = 3306;
-
-    strcpy(query, "select * from Book");
-
-    conn = mysql_init(NULL);
-    
-    if(mysql_real_connect(conn, host, user, passwd, db, port, NULL, 0)) {
-        orintf("MySQL 연결 성공\n");
-    }else{
-        printf("MYSQL 연결 실패\n");
-    }
-    if(mysql_query(conn, query)){
-        printf("쿼리 실패\n");
-    }
-    
-    res = mysql_store_result(conn);
-    while(row = mysql_fetch_row(res)){
-        printf("%s\n", row[0]);
-        printf("%s\n", row[1]);
-        printf("%s\n", row[2]);
-        printf("%s\n", row[3]);
     }
     mysql_close(conn);
     return 0;
