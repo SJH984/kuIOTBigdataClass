760e53b357741e3c1145b0012555d9c545fd59f7
SHJ984 <dokdae0624@naver.com>
2024-03-15 14:49:13 +0900

    cvcnbcnn

---
+++ c_src/String/myString.h
@@ -0,0 +1,8 @@
+#ifndef MYSTRING.H
+#define  MYSTRING.H
+
+void my_strcat(char *des, char *src);
+void my_strcpy(char *des, char *src);
+int my_strcmp(char *s1, char *s2);
+int my_strlen(char *s1);
+#endif
